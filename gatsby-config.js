6ded1163efaebb520fce85ef7a981076fecda974
Pietro Bongiovanni <pietro@fixico.nl>
2019-04-30 19:13:28 +0200

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,5 +123,19 @@ module.exports = {
         icon: `src/images/hello-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-78188164-4',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 }
